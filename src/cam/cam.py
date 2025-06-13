2e43c5f0ff6ed3a05c41d4106d37815ed4ecae08
Gabriel Maia <gabrielmaia6743@gmail.com>
2025-06-13 13:42:48 -0300

    implementação de captura de tela

--- src/cam/cam.py
+++ src/cam/cam.py
@@ -1,64 +1,56 @@
 import os
+from turtle import screensize
 import cv2
 import numpy as np
 from keras.models import load_model
 from ultralytics import YOLO
+import collections
+import pyautogui
 
-# Função que carrega os modelos que treinamos e o Yolo
 def get_models():
     emotion_labels = ['disgust', 'fear', 'neutral', 'surprise']
     # emotion_labels = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad','surprise']
 
-    # Caminhos dos modelos
     model_paths = {
         'yolo': 'models/cam/yolov8n-face.pt',
-        'mini_xception406Boazinha': 'models/tests/AffectnetGray/AffectnetGray_406/Best_28_loss_1.1227_acc_0.59.keras',
-        'mini_xception410Ruim': 'models/tests/AffectnetGray/AffectnetGray_410/Best_16_loss_1.1006_acc_0.59.keras',
-        'mini_xception12': 'models/tests/AffectnetGray/AffectnetGray_12/Best_20_loss_1.1719_acc_0.58.keras',
-        'mini_xception5Boazinha': 'models/tests/Fer2013AffectnetGray/Fer2013AffectnetGray_5/Best_11_loss_1.1520_acc_0.57.keras',
-        'mini_xception477': 'models/tests/AffectnetGray/AffectnetGray_477/Best_9_loss_1.0983_acc_0.56.keras',
-        'mini_xception479': 'models/tests/AffectnetGray/AffectnetGray_479/Best_10_loss_1.1092_acc_0.57.keras',
-        'mini_xception480': 'models/tests/AffectnetGray/AffectnetGray_480/Best_16_loss_1.1995_acc_0.57.keras',
-        'mini_xception481': 'models/tests/AffectnetGray/AffectnetGray_481/Best_16_loss_1.1472_acc_0.56.keras',
-        'mini_xception37': 'models/tests/RAF-DB/RAF-DB_38/Best_255_loss_0.1820_acc_0.96.keras',
-        'mini_xception0': 'models/tests/RAF-DB2/RAF-DB2_0/Best_123_loss_0.4437_acc_0.87.keras'
+        'mini_xceptionEmotion4': 'models/tests/RAF-DB/RAF-DB_38/Best_255_loss_0.1820_acc_0.96.keras',
+        'mini_xceptionEmotion7': 'models/tests/RAF-DB2/RAF-DB2_0/Best_123_loss_0.4437_acc_0.87.keras'
     }
 
-    # Carregando modelos
     model_face = YOLO(model_paths['yolo'])
-    model_mini_xception = load_model(model_paths['mini_xception0'])
+    model_mini_xception = load_model(model_paths['mini_xceptionEmotion4'])
 
     return model_face, model_mini_xception, emotion_labels
 
-# Função que criar as pastas e salvas as imagens dos rostos reconheicdos, com cor e gray
-def save_dataset(img, img_gray, name, name_gray, img_id, emotion):
+def save_dataset(img, img_gray, name, name_gray, emotion):
     dirColor = f'data/{name}/Color/{emotion}'
     dirGray = f'data/{name}/Gray/{emotion}'
+
+    os.makedirs(dirColor, exist_ok=True)
+    arquivos = [f for f in os.listdir(dirColor) if os.path.isfile(os.path.join(dirColor, f))]
+    img_id = len(arquivos) + 1
+    print(img_id)
     
-    # Define os caminhos completos
     paths = {
         'color': dirColor,
         'gray': dirGray
     }
     
-    # Cria os diretórios se não existirem
     for path in paths.values():
         os.makedirs(path, exist_ok=True)
-        # print(f"Pasta '{path}' criada com sucesso!")
     
-    # Gera os nomes dos arquivos
     filenames = {
         'color': f"{name}.{img_id}.jpg",
         'gray': f"{name_gray}.{img_id}.jpg"
     }
     
-    # Salva as imagens
     cv2.imwrite(os.path.join(paths['color'], filenames['color']), img)
     cv2.imwrite(os.path.join(paths['gray'], filenames['gray']), img_gray)
 
-# Função que detecta o nosso rosto e dis qual sentimento predominante
-def detect(img, name, img_id, models):
+def detect(img, name, models):
     model_face, model_mini_xception, emotion_labels = models
+
+    emotion = ''
 
     results = model_face(img, verbose=False, conf=0.5)
 
@@ -68,67 +60,103 @@ def detect(img, name, img_id, models):
         boxes = result.boxes.xyxy.cpu().numpy() 
 
         for box in boxes:
-            # Pega as coordenadas xy próxima dos rostos
             x1, y1, x2, y2 = map(int, box[:4])
+            x1 -= 50
+            y1 -= 50
+            x2 += 50
+            y2 += 50
+            print(x1,y1,x2,y2)
 
-            # Cria um box ao redor do nosso rosto
             cv2.rectangle(img, (x1, y1), (x2, y2), color['blue'], 2)
             
-            # Realiza um recorte somente da área do rosto
             roi_img = img[y1:y2, x1:x2]
             
             if roi_img.size == 0:
                 continue 
             
-            # Aplicando o preprocessamento para noso modelo consequir analisar a imagem
             roi_gray = cv2.cvtColor(roi_img, cv2.COLOR_BGR2GRAY)
             roi_resized = cv2.resize(roi_gray, (75, 75))
-            roi_resized = roi_resized.astype('float32')  # Adicione esta linha
-            cv2.imwrite('debug_detect.png', roi_resized)  # Salva o ROI processado
             roi_normalized = roi_resized / 255.0
             roi_reshaped = np.expand_dims(roi_normalized, axis=(0, -1))
-            print(roi_reshaped.shape)
             
-            # Modelo nosso devolvendo nossa probabilidade e qual emoção é
             probabilities = model_mini_xception.predict(roi_reshaped)
-            print(img_id)
-            print(probabilities)
             predicted_class = np.argmax(probabilities)
-            print(predicted_class)
             emotion = emotion_labels[predicted_class]
-            print(emotion)
             confidence = np.max(probabilities) * 100
             
-            # Printando na câmera a emoção e a probabilidade
             cv2.putText(img, f"Emocao: {emotion}", (x1, y1 - 35), 
                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, color['blue'], 2)
             cv2.putText(img, f"Confianca: {confidence:.1f}%", (x1, y1 - 10), 
                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, color['blue'], 2)
             
-            # Salvando imagens para uma analise humana
-            save_dataset(roi_img, roi_gray, name, name+'_Gray', img_id, emotion)
+            save_dataset(roi_img, roi_gray, name, name+'_Gray', emotion)
 
-    return img
+    return img, emotion
 
+def myScreenshot(numberFeedback, isExec, fps, frameBuffer, screenSize, emotion, dir):
+    tela = pyautogui.screenshot()
+    frame = cv2.cvtColor(np.array(tela), cv2.COLOR_RGB2BGR)
+
+    frameBuffer.append(frame)
+
+    if not isExec[0]:
+        isExec[0] = True
+        if emotion == 'fear':
+            numberFeedback[0] += 1
+            
+            fourcc = cv2.VideoWriter_fourcc(*"XVID")
+            video = cv2.VideoWriter(f'{dir}/feedback{numberFeedback[0]}.avi', fourcc, fps, screenSize)
+
+            for frameVideo in frameBuffer:
+                video.write(frameVideo)
+            
+            video.release()
+            frameBuffer.clear()
+
+    if emotion != 'fear':
+        isExec[0] = False
+    
+
+import time
 
 def main():
     models = get_models()
 
-    img_id = 0
+    name = 'gab'
+    
+    dir = f'Feedbacks/{name}'
+    os.makedirs(dir, exist_ok=True)
+    arquivos = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))]
+
+    numberFeedback = [len(arquivos)]
+    isExec = [False]
+    fps = 5
+    duracao = 180
+    bufferSize = fps * duracao
+    frameBuffer = collections.deque(maxlen=bufferSize)
 
     video_capture = cv2.VideoCapture(0)
+    screenSize = tuple(pyautogui.size())
 
-    # name = input(   '--------------------------------------' \
-    #                 '--------------------------------------' \
-    #                 '--------------------------------------\n' \
-    #                 'Digite seu nome: '                             )
+    inicialTime = time.time()
+    fpsList = []
 
-    name = 'gab'
     while True:
         _, img = video_capture.read()
-        img = detect(img, name, img_id, models)
+        img, emotion = detect(img, name, models)
         cv2.imshow("Detector de Emocao", img)
-        img_id += 1
+
+        myScreenshot(numberFeedback, isExec, fps, frameBuffer, screenSize, emotion, dir)
+
+        endTime = time.time()
+        dt = endTime - inicialTime
+        inicialTime = endTime
+        if dt > 0:
+            fpsInstant = 1 / dt
+            fpsList.append(fpsInstant)
+            if len(fpsList) > 30:
+                fpsList.pop(0)
+            fps = sum(fpsList) / len(fpsList)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
