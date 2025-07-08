5359a8c2059d382bb85ebf9ab7a7c8666bd9f38d
Gabriel Maia <gabrielmaia6743@gmail.com>
2025-07-08 14:42:16 -0300

    mais testes (fer)

--- script.bat
+++ script.bat
@@ -1,155 +1,155 @@
 @echo off
 call venv/Scripts/activate
-py -m src.main 0.0001 0.5 1e-7 0.5
-py -m src.main 0.0001 0.6 1e-7 0.1
-py -m src.main 0.0001 0.6 1e-7 0.2
-py -m src.main 0.0001 0.6 1e-7 0.3
-py -m src.main 0.0001 0.6 1e-7 0.4
-py -m src.main 0.0001 0.6 1e-7 0.5
-py -m src.main 0.0001 0.7 1e-7 0.1
-py -m src.main 0.0001 0.7 1e-7 0.2
-py -m src.main 0.0001 0.7 1e-7 0.3
-py -m src.main 0.0001 0.7 1e-7 0.4
-py -m src.main 0.0001 0.7 1e-7 0.5
-py -m src.main 0.0001 0.8 1e-7 0.1
-py -m src.main 0.0001 0.8 1e-7 0.2
-py -m src.main 0.0001 0.8 1e-7 0.3
-py -m src.main 0.0001 0.8 1e-7 0.4
-py -m src.main 0.0001 0.8 1e-7 0.5
-py -m src.main 0.001 0.1 1e-7 0.1
-py -m src.main 0.001 0.1 1e-7 0.2
-py -m src.main 0.001 0.1 1e-7 0.3
-py -m src.main 0.001 0.1 1e-7 0.4
-py -m src.main 0.001 0.1 1e-7 0.5
-py -m src.main 0.001 0.2 1e-7 0.1
-py -m src.main 0.001 0.2 1e-7 0.2
-py -m src.main 0.001 0.2 1e-7 0.3
-py -m src.main 0.001 0.2 1e-7 0.4
-py -m src.main 0.001 0.2 1e-7 0.5
-py -m src.main 0.001 0.3 1e-7 0.1
-py -m src.main 0.001 0.3 1e-7 0.2
-py -m src.main 0.001 0.3 1e-7 0.3
-py -m src.main 0.001 0.3 1e-7 0.4
-py -m src.main 0.001 0.3 1e-7 0.5
-py -m src.main 0.001 0.4 1e-7 0.1
-py -m src.main 0.001 0.4 1e-7 0.2
-py -m src.main 0.001 0.4 1e-7 0.3
-py -m src.main 0.001 0.4 1e-7 0.4
-py -m src.main 0.001 0.4 1e-7 0.5
-py -m src.main 0.001 0.5 1e-7 0.1
-py -m src.main 0.001 0.5 1e-7 0.2
-py -m src.main 0.001 0.5 1e-7 0.3
-py -m src.main 0.001 0.5 1e-7 0.4
-py -m src.main 0.001 0.5 1e-7 0.5
-py -m src.main 0.001 0.6 1e-7 0.1
-py -m src.main 0.001 0.6 1e-7 0.2
-py -m src.main 0.001 0.6 1e-7 0.3
-py -m src.main 0.001 0.6 1e-7 0.4
-py -m src.main 0.001 0.6 1e-7 0.5
-py -m src.main 0.001 0.7 1e-7 0.1
-py -m src.main 0.001 0.7 1e-7 0.2
-py -m src.main 0.001 0.7 1e-7 0.3
-py -m src.main 0.001 0.7 1e-7 0.4
-py -m src.main 0.001 0.7 1e-7 0.5
-py -m src.main 0.001 0.8 1e-7 0.1
-py -m src.main 0.001 0.8 1e-7 0.2
-py -m src.main 0.001 0.8 1e-7 0.3
-py -m src.main 0.001 0.8 1e-7 0.4
-py -m src.main 0.001 0.8 1e-7 0.5
-py -m src.main 0.005 0.1 1e-7 0.1
-py -m src.main 0.005 0.1 1e-7 0.2
-py -m src.main 0.005 0.1 1e-7 0.3
-py -m src.main 0.005 0.1 1e-7 0.4
-py -m src.main 0.005 0.1 1e-7 0.5
-py -m src.main 0.005 0.2 1e-7 0.1
-py -m src.main 0.005 0.2 1e-7 0.2
-py -m src.main 0.005 0.2 1e-7 0.3
-py -m src.main 0.005 0.2 1e-7 0.4
-py -m src.main 0.005 0.2 1e-7 0.5
-py -m src.main 0.005 0.3 1e-7 0.1
-py -m src.main 0.005 0.3 1e-7 0.2
-py -m src.main 0.005 0.3 1e-7 0.3
-py -m src.main 0.005 0.3 1e-7 0.4
-py -m src.main 0.005 0.3 1e-7 0.5
-py -m src.main 0.005 0.4 1e-7 0.1
-py -m src.main 0.005 0.4 1e-7 0.2
-py -m src.main 0.005 0.4 1e-7 0.3
-py -m src.main 0.005 0.4 1e-7 0.4
-py -m src.main 0.005 0.4 1e-7 0.5
-py -m src.main 0.005 0.5 1e-7 0.1
-py -m src.main 0.005 0.5 1e-7 0.2
-py -m src.main 0.005 0.5 1e-7 0.3
-py -m src.main 0.005 0.5 1e-7 0.4
-py -m src.main 0.005 0.5 1e-7 0.5
-py -m src.main 0.005 0.6 1e-7 0.1
-py -m src.main 0.005 0.6 1e-7 0.2
-py -m src.main 0.005 0.6 1e-7 0.3
-py -m src.main 0.005 0.6 1e-7 0.4
-py -m src.main 0.005 0.6 1e-7 0.5
-py -m src.main 0.005 0.7 1e-7 0.1
-py -m src.main 0.005 0.7 1e-7 0.2
-py -m src.main 0.005 0.7 1e-7 0.3
-py -m src.main 0.005 0.7 1e-7 0.4
-py -m src.main 0.005 0.7 1e-7 0.5
-py -m src.main 0.005 0.8 1e-7 0.1
-py -m src.main 0.005 0.8 1e-7 0.2
-py -m src.main 0.005 0.8 1e-7 0.3
-py -m src.main 0.005 0.8 1e-7 0.4
-py -m src.main 0.005 0.8 1e-7 0.5
-py -m src.main 0.01 0.1 1e-7 0.1
-py -m src.main 0.01 0.1 1e-7 0.2
-py -m src.main 0.01 0.1 1e-7 0.3
-py -m src.main 0.01 0.1 1e-7 0.4
-py -m src.main 0.01 0.1 1e-7 0.5
-py -m src.main 0.01 0.2 1e-7 0.1
-py -m src.main 0.01 0.2 1e-7 0.2
-py -m src.main 0.01 0.2 1e-7 0.3
-py -m src.main 0.01 0.2 1e-7 0.4
-py -m src.main 0.01 0.2 1e-7 0.5
-py -m src.main 0.01 0.3 1e-7 0.1
-py -m src.main 0.01 0.3 1e-7 0.2
-py -m src.main 0.01 0.3 1e-7 0.3
-py -m src.main 0.01 0.3 1e-7 0.4
-py -m src.main 0.01 0.3 1e-7 0.5
-py -m src.main 0.01 0.4 1e-7 0.1
-py -m src.main 0.01 0.4 1e-7 0.2
-py -m src.main 0.01 0.4 1e-7 0.3
-py -m src.main 0.01 0.4 1e-7 0.4
-py -m src.main 0.01 0.4 1e-7 0.5
-py -m src.main 0.01 0.5 1e-7 0.1
-py -m src.main 0.01 0.5 1e-7 0.2
-py -m src.main 0.01 0.5 1e-7 0.3
-py -m src.main 0.01 0.5 1e-7 0.4
-py -m src.main 0.01 0.5 1e-7 0.5
-py -m src.main 0.01 0.6 1e-7 0.1
-py -m src.main 0.01 0.6 1e-7 0.2
-py -m src.main 0.01 0.6 1e-7 0.3
-py -m src.main 0.01 0.6 1e-7 0.4
-py -m src.main 0.01 0.6 1e-7 0.5
-py -m src.main 0.01 0.7 1e-7 0.1
-py -m src.main 0.01 0.7 1e-7 0.2
-py -m src.main 0.01 0.7 1e-7 0.3
-py -m src.main 0.01 0.7 1e-7 0.4
-py -m src.main 0.01 0.7 1e-7 0.5
-py -m src.main 0.01 0.8 1e-7 0.1
-py -m src.main 0.01 0.8 1e-7 0.2
-py -m src.main 0.01 0.8 1e-7 0.3
-py -m src.main 0.01 0.8 1e-7 0.4
-py -m src.main 0.01 0.8 1e-7 0.5
-py -m src.main 0.05 0.1 1e-7 0.1
-py -m src.main 0.05 0.1 1e-7 0.2
-py -m src.main 0.05 0.1 1e-7 0.3
-py -m src.main 0.05 0.1 1e-7 0.4
-py -m src.main 0.05 0.1 1e-7 0.5
-py -m src.main 0.05 0.2 1e-7 0.1
-py -m src.main 0.05 0.2 1e-7 0.2
-py -m src.main 0.05 0.2 1e-7 0.3
-py -m src.main 0.05 0.2 1e-7 0.4
-py -m src.main 0.05 0.2 1e-7 0.5
-py -m src.main 0.05 0.3 1e-7 0.1
-py -m src.main 0.05 0.3 1e-7 0.2
-py -m src.main 0.05 0.3 1e-7 0.3
-py -m src.main 0.05 0.3 1e-7 0.4
+@REM py -m src.main 0.0001 0.5 1e-7 0.5
+@REM py -m src.main 0.0001 0.6 1e-7 0.1
+@REM py -m src.main 0.0001 0.6 1e-7 0.2
+@REM py -m src.main 0.0001 0.6 1e-7 0.3
+@REM py -m src.main 0.0001 0.6 1e-7 0.4
+@REM py -m src.main 0.0001 0.6 1e-7 0.5
+@REM py -m src.main 0.0001 0.7 1e-7 0.1
+@REM py -m src.main 0.0001 0.7 1e-7 0.2
+@REM py -m src.main 0.0001 0.7 1e-7 0.3
+@REM py -m src.main 0.0001 0.7 1e-7 0.4
+@REM py -m src.main 0.0001 0.7 1e-7 0.5
+@REM py -m src.main 0.0001 0.8 1e-7 0.1
+@REM py -m src.main 0.0001 0.8 1e-7 0.2
+@REM py -m src.main 0.0001 0.8 1e-7 0.3
+@REM py -m src.main 0.0001 0.8 1e-7 0.4
+@REM py -m src.main 0.0001 0.8 1e-7 0.5
+@REM py -m src.main 0.001 0.1 1e-7 0.1
+@REM py -m src.main 0.001 0.1 1e-7 0.2
+@REM py -m src.main 0.001 0.1 1e-7 0.3
+@REM py -m src.main 0.001 0.1 1e-7 0.4
+@REM py -m src.main 0.001 0.1 1e-7 0.5
+@REM py -m src.main 0.001 0.2 1e-7 0.1
+@REM py -m src.main 0.001 0.2 1e-7 0.2
+@REM py -m src.main 0.001 0.2 1e-7 0.3
+@REM py -m src.main 0.001 0.2 1e-7 0.4
+@REM py -m src.main 0.001 0.2 1e-7 0.5
+@REM py -m src.main 0.001 0.3 1e-7 0.1
+@REM py -m src.main 0.001 0.3 1e-7 0.2
+@REM py -m src.main 0.001 0.3 1e-7 0.3
+@REM py -m src.main 0.001 0.3 1e-7 0.4
+@REM py -m src.main 0.001 0.3 1e-7 0.5
+@REM py -m src.main 0.001 0.4 1e-7 0.1
+@REM py -m src.main 0.001 0.4 1e-7 0.2
+@REM py -m src.main 0.001 0.4 1e-7 0.3
+@REM py -m src.main 0.001 0.4 1e-7 0.4
+@REM py -m src.main 0.001 0.4 1e-7 0.5
+@REM py -m src.main 0.001 0.5 1e-7 0.1
+@REM py -m src.main 0.001 0.5 1e-7 0.2
+@REM py -m src.main 0.001 0.5 1e-7 0.3
+@REM py -m src.main 0.001 0.5 1e-7 0.4
+@REM py -m src.main 0.001 0.5 1e-7 0.5
+@REM py -m src.main 0.001 0.6 1e-7 0.1
+@REM py -m src.main 0.001 0.6 1e-7 0.2
+@REM py -m src.main 0.001 0.6 1e-7 0.3
+@REM py -m src.main 0.001 0.6 1e-7 0.4
+@REM py -m src.main 0.001 0.6 1e-7 0.5
+@REM py -m src.main 0.001 0.7 1e-7 0.1
+@REM py -m src.main 0.001 0.7 1e-7 0.2
+@REM py -m src.main 0.001 0.7 1e-7 0.3
+@REM py -m src.main 0.001 0.7 1e-7 0.4
+@REM py -m src.main 0.001 0.7 1e-7 0.5
+@REM py -m src.main 0.001 0.8 1e-7 0.1
+@REM py -m src.main 0.001 0.8 1e-7 0.2
+@REM py -m src.main 0.001 0.8 1e-7 0.3
+@REM py -m src.main 0.001 0.8 1e-7 0.4
+@REM py -m src.main 0.001 0.8 1e-7 0.5
+@REM py -m src.main 0.005 0.1 1e-7 0.1
+@REM py -m src.main 0.005 0.1 1e-7 0.2
+@REM py -m src.main 0.005 0.1 1e-7 0.3
+@REM py -m src.main 0.005 0.1 1e-7 0.4
+@REM py -m src.main 0.005 0.1 1e-7 0.5
+@REM py -m src.main 0.005 0.2 1e-7 0.1
+@REM py -m src.main 0.005 0.2 1e-7 0.2
+@REM py -m src.main 0.005 0.2 1e-7 0.3
+@REM py -m src.main 0.005 0.2 1e-7 0.4
+@REM py -m src.main 0.005 0.2 1e-7 0.5
+@REM py -m src.main 0.005 0.3 1e-7 0.1
+@REM py -m src.main 0.005 0.3 1e-7 0.2
+@REM py -m src.main 0.005 0.3 1e-7 0.3
+@REM py -m src.main 0.005 0.3 1e-7 0.4
+@REM py -m src.main 0.005 0.3 1e-7 0.5
+@REM py -m src.main 0.005 0.4 1e-7 0.1
+@REM py -m src.main 0.005 0.4 1e-7 0.2
+@REM py -m src.main 0.005 0.4 1e-7 0.3
+@REM py -m src.main 0.005 0.4 1e-7 0.4
+@REM py -m src.main 0.005 0.4 1e-7 0.5
+@REM py -m src.main 0.005 0.5 1e-7 0.1
+@REM py -m src.main 0.005 0.5 1e-7 0.2
+@REM py -m src.main 0.005 0.5 1e-7 0.3
+@REM py -m src.main 0.005 0.5 1e-7 0.4
+@REM py -m src.main 0.005 0.5 1e-7 0.5
+@REM py -m src.main 0.005 0.6 1e-7 0.1
+@REM py -m src.main 0.005 0.6 1e-7 0.2
+@REM py -m src.main 0.005 0.6 1e-7 0.3
+@REM py -m src.main 0.005 0.6 1e-7 0.4
+@REM py -m src.main 0.005 0.6 1e-7 0.5
+@REM py -m src.main 0.005 0.7 1e-7 0.1
+@REM py -m src.main 0.005 0.7 1e-7 0.2
+@REM py -m src.main 0.005 0.7 1e-7 0.3
+@REM py -m src.main 0.005 0.7 1e-7 0.4
+@REM py -m src.main 0.005 0.7 1e-7 0.5
+@REM py -m src.main 0.005 0.8 1e-7 0.1
+@REM py -m src.main 0.005 0.8 1e-7 0.2
+@REM py -m src.main 0.005 0.8 1e-7 0.3
+@REM py -m src.main 0.005 0.8 1e-7 0.4
+@REM py -m src.main 0.005 0.8 1e-7 0.5
+@REM py -m src.main 0.01 0.1 1e-7 0.1
+@REM py -m src.main 0.01 0.1 1e-7 0.2
+@REM py -m src.main 0.01 0.1 1e-7 0.3
+@REM py -m src.main 0.01 0.1 1e-7 0.4
+@REM py -m src.main 0.01 0.1 1e-7 0.5
+@REM py -m src.main 0.01 0.2 1e-7 0.1
+@REM py -m src.main 0.01 0.2 1e-7 0.2
+@REM py -m src.main 0.01 0.2 1e-7 0.3
+@REM py -m src.main 0.01 0.2 1e-7 0.4
+@REM py -m src.main 0.01 0.2 1e-7 0.5
+@REM py -m src.main 0.01 0.3 1e-7 0.1
+@REM py -m src.main 0.01 0.3 1e-7 0.2
+@REM py -m src.main 0.01 0.3 1e-7 0.3
+@REM py -m src.main 0.01 0.3 1e-7 0.4
+@REM py -m src.main 0.01 0.3 1e-7 0.5
+@REM py -m src.main 0.01 0.4 1e-7 0.1
+@REM py -m src.main 0.01 0.4 1e-7 0.2
+@REM py -m src.main 0.01 0.4 1e-7 0.3
+@REM py -m src.main 0.01 0.4 1e-7 0.4
+@REM py -m src.main 0.01 0.4 1e-7 0.5
+@REM py -m src.main 0.01 0.5 1e-7 0.1
+@REM py -m src.main 0.01 0.5 1e-7 0.2
+@REM py -m src.main 0.01 0.5 1e-7 0.3
+@REM py -m src.main 0.01 0.5 1e-7 0.4
+@REM py -m src.main 0.01 0.5 1e-7 0.5
+@REM py -m src.main 0.01 0.6 1e-7 0.1
+@REM py -m src.main 0.01 0.6 1e-7 0.2
+@REM py -m src.main 0.01 0.6 1e-7 0.3
+@REM py -m src.main 0.01 0.6 1e-7 0.4
+@REM py -m src.main 0.01 0.6 1e-7 0.5
+@REM py -m src.main 0.01 0.7 1e-7 0.1
+@REM py -m src.main 0.01 0.7 1e-7 0.2
+@REM py -m src.main 0.01 0.7 1e-7 0.3
+@REM py -m src.main 0.01 0.7 1e-7 0.4
+@REM py -m src.main 0.01 0.7 1e-7 0.5
+@REM py -m src.main 0.01 0.8 1e-7 0.1
+@REM py -m src.main 0.01 0.8 1e-7 0.2
+@REM py -m src.main 0.01 0.8 1e-7 0.3
+@REM py -m src.main 0.01 0.8 1e-7 0.4
+@REM py -m src.main 0.01 0.8 1e-7 0.5
+@REM py -m src.main 0.05 0.1 1e-7 0.1
+@REM py -m src.main 0.05 0.1 1e-7 0.2
+@REM py -m src.main 0.05 0.1 1e-7 0.3
+@REM py -m src.main 0.05 0.1 1e-7 0.4
+@REM py -m src.main 0.05 0.1 1e-7 0.5
+@REM py -m src.main 0.05 0.2 1e-7 0.1
+@REM py -m src.main 0.05 0.2 1e-7 0.2
+@REM py -m src.main 0.05 0.2 1e-7 0.3
+@REM py -m src.main 0.05 0.2 1e-7 0.4
+@REM py -m src.main 0.05 0.2 1e-7 0.5
+@REM py -m src.main 0.05 0.3 1e-7 0.1
+@REM py -m src.main 0.05 0.3 1e-7 0.2
+@REM py -m src.main 0.05 0.3 1e-7 0.3
+@REM py -m src.main 0.05 0.3 1e-7 0.4
 py -m src.main 0.05 0.3 1e-7 0.5
 py -m src.main 0.05 0.4 1e-7 0.1
 py -m src.main 0.05 0.4 1e-7 0.2
